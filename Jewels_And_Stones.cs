cd01c1832d4f537a13b9ba0e854342ad73a3d569
Jayam Malviya <malviya.jayam@gmail.com>
2018-04-11 15:53:32 -0700

    Jewels_And_Stones.cs

---
+++ Jewels_And_Stones.cs
@@ -0,0 +1,29 @@
+public class Solution {
+
+
+// You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+// The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+// Example 1:
+
+// Input: J = "aA", S = "aAAbbbb"
+// Output: 3
+
+// Example 2:
+// Input: J = "z", S = "ZZ"
+// Output: 0
+// Note:
+// S and J will consist of letters and have length at most 50.
+// The characters in J are distinct.
+
+    public int NumJewelsInStones(string j, string s) {
+        if(j == null || s.Length == 0 || s == null) return 0;
+        var set = new HashSet<char>();
+        foreach(char c in j)
+            set.Add(c);
+        int count = 0;
+        foreach(char c in s)
+            if(set.Contains(c)) count++;
+        return count;
+    }
+}
