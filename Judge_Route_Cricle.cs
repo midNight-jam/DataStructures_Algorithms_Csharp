69bff64c68ec3c7d07fb03b4e50ab57d8f454591
midNight-jam <malviya.jayam@gmail.com>
2018-04-12 12:46:13 +0530

    Judge_Route_Cricle.cs

---
+++ Judge_Route_Cricle.cs
@@ -0,0 +1,32 @@
+
+// 657. Judge Route Circle
+// DescriptionHintsSubmissionsDiscussSolution
+// Initially, there is a Robot at position (0, 0). Given a sequence of its moves, judge if this robot makes a circle, which means it moves back to the original place.
+// The move sequence is represented by a string. And each move is represent by a character. The valid robot moves are R (Right), L (Left), U (Up) and D (down). The output should be true or false representing whether the robot makes a circle.
+// Example 1:
+// Input: "UD"
+// Output: true
+// Example 2:
+// Input: "LL"
+// Output: false
+
+public class Solution {
+    public bool JudgeCircle(string moves) {
+      if(moves == null || moves.Length == 0) return false;
+      int x = 0;
+      int y = 0;
+      foreach(char c in moves)
+        switch(c){
+          case 'U': y++;
+            break;
+          case 'D': y--;
+            break;
+          case 'R': x++;
+            break;
+          case  'L': x--;
+            break;
+        }
+      
+      return (x  == 0 && y == 0);
+    }
+}
