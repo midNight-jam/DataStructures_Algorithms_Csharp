98799f5b740c6b37b84713cfffce0dff1c28315c
jayam.malviya <jayam.malviya@servicenow.com>
2018-04-16 22:50:23 -0700

    Reverse_String.cs

---
+++ DSA_Proj/DSA_Proj/Number_of_Lines_To_Write_String.cs
@@ -0,0 +1,30 @@
+using System;
+
+public class Solution {
+    public static int[] NumberOfLines(int[] widths, string S) {
+        if(widths == null || widths.Length == 0 || S == null || S.Length == 0) return new int[0];
+        int lines= 1;
+        int width = 0;
+        
+        foreach(char c in S)
+        	if(widths[c-'a'] + width >= 100){
+        		lines++;
+        		width = widths[c-'a'] + width == 100 ? 0 : widths[c-'a'];
+        	}else
+        		width += widths[c-'a'];
+        	
+        
+        return new int[]{lines, width}; 
+    }
+
+    public static void Main(string [] args){
+    	//int [] widths = new int[]{4,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+    	//string S = "bbbcccdddaaa";
+    	int [] widths = new int[]{10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10};
+    	string S = "abcdefghijklmnopqrstuvwxyz";
+
+    	int[] res = NumberOfLines(widths, S);
+    	foreach(int i in res)
+    		Console.WriteLine(i);
+    }
+}
---
+++ DSA_Proj/DSA_Proj/Reverse_String.cs
@@ -0,0 +1,21 @@
+using System;
+
+/*
+344. Reverse String
+Write a function that takes a string as input and returns the string reversed.
+Example:
+Given s = "hello", return "olleh
+*/
+
+public class Solution {
+    public string ReverseString(string s) {
+        if(s == null || s.Length == 0) return s;
+        char [] arr = s.ToCharArray();
+        for(int i = 0, j = s.Length - 1; i < j; i++, j--){
+            char temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+        return new string(arr);
+    }
+}
